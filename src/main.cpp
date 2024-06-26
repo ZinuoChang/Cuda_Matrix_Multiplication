24fcb9d1aa67d254144bceec54bfe3c1bbe29948
Zinuo <benjaminchang0731@gmail.com>
2024-06-26 19:52:37 -0400

    update

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,12 @@
 #include <Eigen/Dense>
 
 int main (void){
+    // Define dimensions
     int rows = 1000;
     int cols = 1000;
     int vec_num = 200;
 
+    // Generate the matrices randomly
     Eigen::MatrixXd matrix = Eigen::MatrixXd::Random(rows, cols);
     Eigen::MatrixXd vectorMatrix(cols, vec_num);
     Eigen::MatrixXd result(rows, vec_num);
@@ -21,42 +23,24 @@ int main (void){
         vectorMatrix.col(i) = vectors[i];
     }
 
-    double* matrix1 = new double[rows * cols];
-    double* vectorMatrix1 = new double[rows * vec_num];
-    double* result1 = new double[rows * vec_num];
+    double* matrix_array = new double[rows * cols];
+    double* vectorMatrix_array = new double[rows * vec_num];
+    double* result_array = new double[rows * vec_num];
 
+    // Assign the value in eigen matrix into array
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
-            matrix1[i * cols + j] = matrix(i, j);
+            matrix_array[i * cols + j] = matrix(i, j);
         }
     }
 
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < vec_num; ++j) {
-            vectorMatrix1[i * vec_num + j] = vectorMatrix(i, j);
+            vectorMatrix_array[i * vec_num + j] = vectorMatrix(i, j);
         }
     }
 
-
-    // std::cout << matrix <<std::endl;
-
-    // for (int i = 0; i < rows; i++) {
-    //     for (int j = 0; j < cols; j++) {
-    //         std::cout << matrix1[i * cols + j] << " ";
-    //     }
-    //     std::cout << std::endl;
-    // }
-
-    // std::cout << vectorMatrix <<std::endl;
-
-    // for (int i = 0; i < rows; i++) {
-    //     for (int j = 0; j < vec_num; j++) {
-    //         std::cout << vectorMatrix1[i * vec_num + j] << " ";
-    //     }
-    //     std::cout << std::endl;
-    // }
-
-    matrixVectorMul(matrix1, vectorMatrix1, result1, rows, cols, vec_num);
+    matrixVectorMul(matrix_array, vectorMatrix_array, result_array, rows, cols, vec_num);
 
     auto start = std::chrono::high_resolution_clock::now();
     
@@ -67,14 +51,14 @@ int main (void){
     std::cout << "Elapsed time Eigen: " << elapsed.count() << " seconds" << std::endl;
 
     
-
+    // Compare the results
     // std::cout << "Eigen" <<std::endl;
     // std::cout << result <<std::endl;
 
     // std::cout << "GPU" <<std::endl;
     // for (int i = 0; i < rows; i++) {
     //     for (int j = 0; j < vec_num; j++) {
-    //         std::cout << result1[i * vec_num + j] << " ";
+    //         std::cout << result_array[i * vec_num + j] << " ";
     //     }
     //     std::cout << std::endl;
     // }
